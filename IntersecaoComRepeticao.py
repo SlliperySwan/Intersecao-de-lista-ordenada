c3b81251b4257fd72dfb3139dbd5d3d2111c9cbb
Eraldo Botelho <eraldobotelhojr@gmail.com>
2025-05-19 17:10:49 -0300

    Main

---
+++ IntersecaoComRepeticao.py
@@ -0,0 +1,30 @@
+import time
+
+def busca_bin(Lista, i, f, num):
+    if i > f:
+        return -1
+    else:
+        m = int((i+f)/2)
+        if  Lista[m] > num:
+            return busca_bin(Lista, i , m-1, num)
+        elif Lista[m] < num:
+            return busca_bin(Lista, m+1, f, num)
+        else:
+            return m
+
+def intersecao2(A, inicioA, fimA, B, inicioB, fimB):
+    if inicioA > fimA or inicioB > fimB:
+        return []
+    else:
+        m = int((inicioA+fimA)/2)
+        v1 = intersecao2(A, inicioA, m - 1, B, inicioB, fimB)
+        v2 = intersecao2(A, m+1, fimA, B, inicioB, fimB)
+        index = busca_bin(B, 0, len(B)-1, A[m])
+        inter= []
+        if index != -1:
+            inter.append(A[m])
+
+        return v1 + inter + v2 
+
+def intersecao(A, B):
+    return intersecao2(A, 0, len(A)- 1, B, 0, len(B)-1)
---
+++ IntersecaoSemRepeticao.py
@@ -0,0 +1,39 @@
+import time
+import copy
+from random import randint as rand
+
+def busca_bin(Lista, i, f, num):
+    if i > f:
+        return []
+    else:
+        m = int((i+f)/2)
+        if  Lista[m] > num:
+            return busca_bin(Lista, i , m-1, num)
+        elif Lista[m] < num:
+            return busca_bin(Lista, m+1, f, num)
+        else:
+            retorno = Lista[m]
+            while True:
+                try:
+                    Lista.remove(retorno)
+                except:
+                    break
+            return [retorno]
+
+def intersecao2(A, inicioA, fimA, B, inicioB, fimB):
+    if inicioA > fimA or inicioB > fimB:
+        return []
+    else:
+        m = int((inicioA+fimA)/2)
+        v1 = intersecao2(A, inicioA, m - 1, B, inicioB, fimB)
+        v2 = intersecao2(A, m+1, fimA, B, inicioB, fimB)
+        index = busca_bin(B, 0, len(B)-1, A[m])
+        return v1 + index + v2 
+
+def intersecao(A, B):
+    return intersecao2(A, 0, len(A)- 1, B, 0, len(B)-1)
+
+
+
+
+
