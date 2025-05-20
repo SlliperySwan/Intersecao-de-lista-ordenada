2aa6c8cfe0f8d3e4d9d2b4cb2c820c3c9a630bf3
Eraldo Botelho <eraldobotelhojr@gmail.com>
2025-05-19 22:00:59 -0300

    Otimização no algoritmo sem repetição
    Não usa mais memória auxiliar
    É um pouco mais rápido, porém mantem a complexidade encontrada na última versão.

--- IntersecaoSemRepeticao.py
+++ IntersecaoSemRepeticao.py
@@ -1,37 +1,31 @@
-import time
-import copy
-
-def busca_bin(Lista, i, f, num):
-    if i > f:
+def busca_bin(L, inicio, fim, numero):
+    if inicio > fim:
         return []
     else:
-        m = int((i+f)/2)
-        if  Lista[m] > num:
-            return busca_bin(Lista, i , m-1, num)
-        elif Lista[m] < num:
-            return busca_bin(Lista, m+1, f, num)
+        meio = (inicio+fim)//2
+        if  L[meio] > numero:
+            return busca_bin(L, inicio , meio-1, numero)
+        elif L[meio] < numero:
+            return busca_bin(L, meio+1, fim, numero)
         else:
-            retorno = Lista[m]
-            while True:
-                try:
-                    Lista.remove(retorno)
-                except:
-                    break
-            return [retorno]
+            return [L[meio]]
 
 def intersecao2(A, inicioA, fimA, B, inicioB, fimB):
     if inicioA > fimA or inicioB > fimB:
         return []
     else:
-        m = int((inicioA+fimA)/2)
-        v1 = intersecao2(A, inicioA, m - 1, B, inicioB, fimB)
-        v2 = intersecao2(A, m+1, fimA, B, inicioB, fimB)
-        index = busca_bin(B, 0, len(B)-1, A[m])
-        return v1 + index + v2 
+        meio = (inicioA+fimA)//2
+        arrayMenor = intersecao2(A, inicioA, meio - 1, B, inicioB, fimB)
+        arrayMaior = intersecao2(A, meio+1, fimA, B, inicioB, fimB)
+        termoComum = busca_bin(B, 0, len(B)-1, A[meio])
+        duplicataMenor = len(arrayMenor) != 0 and termoComum[0] <= arrayMenor[-1]
+        duplicataMaior = len(arrayMaior) != 0 and termoComum[0] >= arrayMaior[0]
+        if duplicataMenor or duplicataMaior:
+            termoComum = []
+        return arrayMenor + termoComum + arrayMaior
 
 def intersecao(A, B):
-    beta = copy.deepcopy(B)
-    return intersecao2(A, 0, len(A)- 1, beta, 0, len(beta)-1)
+    return intersecao2(A, 0, len(A)- 1, B, 0, len(B)-1)
 
 
 
