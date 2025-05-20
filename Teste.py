7f4d3d41e0d029f73a329d336a1107e2ac8cea51
João Coutinho <joaovcoutinho113@gmail.com>
2025-05-20 17:59:52 -0300

    Update Teste.py

--- Teste.py
+++ Teste.py
@@ -3,7 +3,22 @@ import random
 from IntersecaoComRepeticao import intersecao as intersecaoR
 from IntersecaoSemRepeticao import intersecao
 
-# Exemplo COM repetição 100 
+# Exemplo COM repetição 
+
+array1 = [1, 3, 4, 5, 7, 9, 11, 11, 12, 15]
+array2 = [1, 2, 2, 5, 6, 10, 10, 11, 12, 20]
+
+inicio_r = time.time()
+resultado_r = intersecaoR(array1, array2)
+fim_r = time.time()
+
+print("Interseção COM repetição 100 elementos")
+print("Array 1:", array1)
+print("Array 2:", array2)
+print("Resultado:", resultado_r)
+print("Tempo: {:.6f} segundos".format(fim_r - inicio_r))
+print("================================================================================")
+
 array1 = sorted(set([i * 4 for i in range(100)] + [i * 5 for i in range(30)]))
 array2 = sorted(set([i * 4 for i in range(100)] + [i * 5 for i in range(30)]))
 
@@ -74,6 +89,21 @@ print("=========================================================================
 print("================================================================================")
 
 # Exemplo SEM repetição
+
+array3 = [1, 3, 4, 5, 7, 9, 10, 13, 15, 17]
+array4 = [1, 2, 4, 6, 8, 10, 12, 15, 16, 20]
+
+inicio_sr = time.time()
+resultado_r2 = intersecao(array3, array4)
+fim_sr = time.time()
+
+print("Interseção SEM repetição 10 elementos")
+print("Array 3:", array3)
+print("Array 4:", array4)
+print("Resultado:", resultado_r2)
+print("Tempo: {:.6f} segundos".format(fim_sr - inicio_sr))
+print("================================================================================")
+
 array3 = sorted(random.sample(range(1, 10001), 100))
 array4 = sorted(random.sample(range(1, 10001), 100))
 
