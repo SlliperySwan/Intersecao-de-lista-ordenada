6cb9a8898ecabe978ac04e4e7cfd0478f42b8c73
Eraldo Botelho <eraldobotelhojr@gmail.com>
2025-05-22 15:17:43 -0300

    Remoção de argumentos
    Alguns argumentos não estavam sendo utilizados.

--- IntersecaoSemRepeticao.py
+++ IntersecaoSemRepeticao.py
@@ -1,23 +1,23 @@
-def busca_bin(L, inicio, fim, numero):
+def buscaBinaria(L, inicio, fim, numero):
     if inicio > fim:
         return []
     else:
-        meio = (inicio+fim)//2
+        meio = (inicio + fim)//2
         if  L[meio] > numero:
-            return busca_bin(L, inicio , meio-1, numero)
+            return buscaBinaria(L, inicio , meio - 1, numero)
         elif L[meio] < numero:
-            return busca_bin(L, meio+1, fim, numero)
+            return buscaBinaria(L, meio + 1, fim, numero)
         else:
             return [L[meio]]
 
-def intersecao2(A, inicioA, fimA, B, inicioB, fimB):
-    if inicioA > fimA or inicioB > fimB:
+def intersecao2(A, inicioA, fimA, B):
+    if inicioA > fimA:
         return []
     else:
-        meio = (inicioA+fimA)//2
-        arrayMenor = intersecao2(A, inicioA, meio - 1, B, inicioB, fimB)
-        arrayMaior = intersecao2(A, meio+1, fimA, B, inicioB, fimB)
-        termoComum = busca_bin(B, 0, len(B)-1, A[meio])
+        meio = (inicioA + fimA)//2
+        arrayMenor = intersecao2(A, inicioA, meio - 1, B)
+        arrayMaior = intersecao2(A, meio + 1, fimA, B)
+        termoComum = buscaBinaria(B, 0, len(B) - 1, A[meio])
         if len(termoComum) != 0:
             existeNoMenor = len(arrayMenor) != 0 and termoComum[0] <= arrayMenor[-1]
             existeNoMaior = len(arrayMaior) != 0 and termoComum[0] >= arrayMaior[0]
@@ -29,6 +29,6 @@ def intersecao(A, B):
     tamA = len(A)
     tamB = len(B)
     if tamB >= tamA:
-        return intersecao2(A, 0, tamA - 1, B, 0, tamB -1)
+        return intersecao2(A, 0, tamA - 1, B)
     else:
-        return intersecao2(B, 0, tamB - 1, A, 0, tamA -1)
+        return intersecao2(B, 0, tamB - 1, A)
